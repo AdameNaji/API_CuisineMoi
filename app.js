e298b311b5a2667720422596c81c1b9c7090e3ec
Adame Naji <adamenaji@hotmail.fr>
2021-02-13 15:27:43 +0100

    - API launch file

--- app.js
+++ app.js
@@ -1,41 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/**
+ * @title : app.js
+ * @author : Adame Naji
+ * @description : Server launch file
+ */
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const port = 3000;
 
-var app = express();
+const router = require("./routes/router");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+/* Launching the server  */
+http.listen(port, function (err) {
+    if (err) {
+        console.error(err);
+    }
+    console.log(`Server listening at http://localhost:${port}`);
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+/* Basic route */
+app.use("/api", router);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
